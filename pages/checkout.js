9fc924373d435ead94b202c1dd90fabeea91fa14
PrincipeGatto <gori.leonardo.leonardo@gmail.com>
2025-08-04 00:02:59 +0200

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,26 +1,28 @@
 // pages/checkout.js
 
 import { useEffect } from 'react';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function Checkout() {
+  const { t } = useTranslation('common');
+
   useEffect(() => {
     fetch('/api/checkout', { method: 'POST' })
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         window.location.href = data.url;
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <p>Reindirizzamento al pagamento…</p>
+      <p>{t('loading')}…</p>
     </div>
   );
 }
 
-// Utilizziamo getServerSideProps per caricare il namespace 'common' ed evitare errori di prerender
 export async function getServerSideProps({ locale }) {
   return {
     props: {
