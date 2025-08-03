e725b827f2b0fe3b5b03162e8ba926ff1d20e7e7
PrincipeGatto <gori.leonardo.leonardo@gmail.com>
2025-08-03 23:23:33 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 // pages/_app.js
 import { appWithTranslation } from 'next-i18next';
-import '../styles/globals.css';
 import nextI18nConfig from '../next-i18next.config';
+import '../styles/globals.css';
+
+// Import del componente
+import LangSwitcher from '../components/LangSwitcher';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      {/* Qui renderizzi il selettore di lingua */}
+      <header className="p-4 flex justify-end">
+        <LangSwitcher />
+      </header>
+
+      {/* Qui tutto il resto della pagina */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-// 'appWithTranslation' collega next-i18next alla tua App
 export default appWithTranslation(MyApp, nextI18nConfig);
