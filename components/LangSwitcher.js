165e5aef44433160e02f6d544f0e169ab1b5a973
PrincipeGatto <gori.leonardo.leonardo@gmail.com>
2025-08-03 23:55:37 +0200

    Update LangSwitcher.js

--- components/LangSwitcher.js
+++ components/LangSwitcher.js
@@ -5,12 +5,16 @@ export default function LangSwitcher() {
   const router = useRouter();
   const { locale, locales, pathname, query, asPath } = router;
 
+  // Durante il prerender o in assenza di routing, non renderizziamo nulla
+  if (!locales) return null;
+
   return (
     <select
       value={locale}
       onChange={(e) =>
         router.push({ pathname, query }, asPath, { locale: e.target.value })
       }
+      className="border p-1 rounded"
     >
       {locales.map((loc) => (
         <option key={loc} value={loc}>
