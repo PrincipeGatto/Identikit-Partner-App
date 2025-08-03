7040f44ece0f102022e579aff89d09abafcfa50b
PrincipeGatto <gori.leonardo.leonardo@gmail.com>
2025-08-03 23:43:33 +0200

    Fix checkout prerender by adding getStaticProps

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,7 @@
 // pages/checkout.js
 
 import { useEffect } from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function Checkout() {
   useEffect(() => {
@@ -17,4 +18,13 @@ export default function Checkout() {
       <p>Reindirizzamento al pagamento…</p>
     </div>
   );
+}
+
+// Punto 1: forziamo il caricamento del namespace 'common' per evitare errori di prerender
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
 }
