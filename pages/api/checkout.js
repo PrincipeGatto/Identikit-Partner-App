47c05fe85eab72d117d4b06c5ff3fcb592976ffb
PrincipeGatto <gori.leonardo.leonardo@gmail.com>
2025-08-03 22:16:23 +0200

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -26,6 +26,6 @@ export default async function handler(req, res) {
     res.status(200).json({ url: session.url });
   } catch (err) {
     console.error('Stripe checkout error:', err);
-    res.status(500).json({ error: 'Errore nella creazione della sessione' });
+    res.status(500).json({ error: 'Errore creazione sessione' });
   }
 }
